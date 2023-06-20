6fccc380c1dbd510ab97e9b145655a533551c86a
Rose <rosekivuvan@gmail.com>
2023-06-20 09:29:48 +0300

    first commit

---
+++ app/src/main/java/com/rosekn/assessment/Login.kt
@@ -0,0 +1,22 @@
+package com.rosekn.assessment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rosekn.assessment.databinding.ActivityLoginBinding
+import com.rosekn.assessment.databinding.ActivityMainBinding
+
+class Login : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            val intent=Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
--- app/src/main/java/com/rosekn/assessment/MainActivity.kt
+++ app/src/main/java/com/rosekn/assessment/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.rosekn.assessment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rosekn.assessment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        validatePage()
+
+
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    fun validatePage(): Boolean {
+        val username = binding.etuser.text.toString().capitalize()
+        val email = binding.etemail.text.toString()
+        val phone = binding.etphone.text.toString()
+        val password = binding.etpassword.text.toString()
+        var error = false
+        clearErrors()
+
+
+        if (username.isBlank()) {
+            binding.etuser.error = "user name is required"
+            error = true
+        }
+
+
+        if (email.isBlank()) {
+            binding.etemail.error = "email name is required"
+            error = true
+        }
+
+        if (phone.isBlank()) {
+            binding.etphone.error = "Contact is required"
+            error = true
+        }
+
+        if (email.isBlank()) {
+            binding.etemail.error = "Email is required"
+            error = true
+        }
+
+
+        return email.isNotBlank() && username.isNotBlank() && phone.isNotBlank() && email.isNotBlank() && password.isNotBlank()
+    }
+
+    fun clearErrors() {
+        binding.etemail.error = null
+        binding.etphone.error = null
+        binding.etpassword.error = null
+        binding.etemail.error = null
     }
 }
